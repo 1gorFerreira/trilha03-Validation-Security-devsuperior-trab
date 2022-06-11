ed9d45432b0b64abff96b6f8d4ca485ddf61e64a
Igor <igorferreiradaniel99@hotmail.com>
2022-06-11 17:58:29 -0300

    H2 config

--- src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds04/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.bds04.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,11 +15,16 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private JwtTokenStore tokenStore;
+	
+	private static final String[] H2_AND_OAUTH = { "/h2-console/**", "/oauth/token" };
 
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", "/events/**", "/cities/**" };
+	private static final String[] PUBLIC = { "/events/**", "/cities/**" };
 
 	private static final String[] CLIENT = { "/events/**"};
 
@@ -28,7 +36,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
+		.antMatchers(H2_AND_OAUTH).permitAll()
 		.antMatchers(HttpMethod.GET, PUBLIC).permitAll()
 		.antMatchers(HttpMethod.POST, CLIENT).hasAnyRole("CLIENT", "ADMIN")
 		.anyRequest().hasAnyRole("ADMIN");
